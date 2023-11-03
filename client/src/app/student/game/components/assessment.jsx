678e04d8e9ebfb20356f4c95d9b6ec9a50a20736
Inesh Loka <56318873+inesh-l@users.noreply.github.com>
2023-11-03 17:57:34 -0400

    Converted game assets to components

---
+++ client/src/app/student/game/components/assessment.jsx
@@ -0,0 +1,9 @@
+export function Assessment() {
+    return (
+        <div>
+            <svg width="143" height="88" viewBox="0 0 143 88" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M1 1.00003C90.6466 4.30554 119.611 24.9255 142 87.5" stroke="black" stroke-dasharray="2 2"/>
+            </svg> 
+        </div>
+    )
+}
