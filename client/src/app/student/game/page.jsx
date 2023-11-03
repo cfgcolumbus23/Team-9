67c332dc815cad884502fe0890c36f9ee983d667
Inesh Loka <56318873+inesh-l@users.noreply.github.com>
2023-11-03 16:45:06 -0400

    Initialize game page branch

--- client/src/app/student/game/page.jsx
+++ client/src/app/student/game/page.jsx
@@ -8,11 +8,13 @@ export default function game() {
 
         <div>
             <h1 class="text-2xl" align="center">World {world}</h1>
+            <div class="h-7"></div>
             <div class="grid grid-cols-12 gap-4">
-                <div>01</div>
-                <div>09</div>
-                <div></div>
-                <div>010</div>
+                {/*12x6 grid for game design*/}
+                <div>1</div>
+                <div>2</div>
+                <div>3</div>
+                <div>4</div>
             </div>
         </div>
     )
