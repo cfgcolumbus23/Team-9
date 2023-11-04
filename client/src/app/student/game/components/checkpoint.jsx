5dd900654e6da43decd7784ab2e507841d869de0
LeoZhang47 <zleo200347@gmail.com>
2023-11-03 23:44:48 -0400

    started styling question buttons

--- client/src/app/student/game/components/checkpoint.jsx
+++ client/src/app/student/game/components/checkpoint.jsx
@@ -1,8 +1,8 @@
 export function Checkpoint(props) {
     let checkpointClass = (props.type == "quiz") ? "rounded-full" : ""; 
     return (
-        <div class={"w-[95px] h-[95px] bg-zinc-300 " + checkpointClass}>
-            Component test
+        <div class={"w-[95px] h-[95px] hover " + props.color + " " + checkpointClass}>
+            {"Question " + props.number}
         </div>
     )
 }
