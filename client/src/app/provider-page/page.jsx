9542ebe55083186412ba49603b943fbc4a344ad2
roryrod40 <rodriguez.rory40@gmail.com>
2023-11-03 21:59:03 -0400

    Implemented the ability to select num of sections

--- client/src/app/provider-page/page.jsx
+++ client/src/app/provider-page/page.jsx
@@ -3,91 +3,184 @@
 import React, { useState } from 'react';
 
 const MyForm = () => {
-  const [formData, setFormData] = useState([
-    { type: 'qa', question: '', answer: '' },
-    { type: 'qa', question: '', answer: '' },
-    { type: 'content', content: '' },
-    { type: 'qa', question: '', answer: '' },
-    { type: 'content', content: '' },
+  const [numSections, setNumSections] = useState(1);
+
+  const [sectionData, setSectionData] = useState([
+    {
+      id: 1,
+      parts: [
+        { type: 'qa', question: '', answer: '' },
+        { type: 'qa', question: '', answer: '' },
+        { type: 'content', content: '' },
+        { type: 'qa', question: '', answer: '' },
+        { type: 'content', content: '' },
+      ],
+    },
   ]);
 
-  const handleInputChange = (index, key, value) => {
-    const updatedData = [...formData];
-    updatedData[index][key] = value;
-    setFormData(updatedData);
+  const handleInputChange = (sectionId, partIndex, key, value) => {
+    const updatedData = sectionData.map((section) =>
+      section.id === sectionId
+        ? {
+            ...section,
+            parts: section.parts.map((part, index) =>
+              index === partIndex ? { ...part, [key]: value } : part
+            ),
+          }
+        : section
+    );
+    setSectionData(updatedData);
   };
 
-  const handleSelectChange = (index, value) => {
-    const updatedData = [...formData];
-    updatedData[index] = { type: value, content: '' };
-    setFormData(updatedData);
+  const handleSelectChange = (sectionId, partIndex, value) => {
+    const updatedData = sectionData.map((section) =>
+      section.id === sectionId
+        ? {
+            ...section,
+            parts: section.parts.map((part, index) =>
+              index === partIndex
+                ? {
+                    ...part,
+                    type: value,
+                    question: '',
+                    answer: '',
+                    content: '',
+                  }
+                : part
+            ),
+          }
+        : section
+    );
+    setSectionData(updatedData);
+  };
+
+  const handleNumSectionsChange = (e) => {
+    const value = parseInt(e.target.value, 10);
+    setNumSections(value >= 1 ? value : 1);
+
+    if (value > sectionData.length) {
+      const newSections = Array.from(
+        { length: value - sectionData.length },
+        (_, index) => ({
+          id: sectionData.length + index + 1,
+          parts: [
+            { type: 'qa', question: '', answer: '' },
+            { type: 'qa', question: '', answer: '' },
+            { type: 'content', content: '' },
+            { type: 'qa', question: '', answer: '' },
+            { type: 'content', content: '' },
+          ],
+        })
+      );
+
+      setSectionData([...sectionData, ...newSections]);
+    } else if (value < sectionData.length) {
+      setSectionData(sectionData.slice(0, value));
+    }
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    // Do something with the formData, e.g., send it to a server
-    console.log(formData);
+    console.log(sectionData);
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      {formData.map((item, index) => (
-        <div key={index}>
-          <label>
-            Type:
-            <select
-              value={item.type}
-              onChange={(e) => handleSelectChange(index, e.target.value)}
-            >
-              <option value="qa">Question & Answer</option>
-              <option value="content">Content</option>
-            </select>
-          </label>
-          {item.type === 'qa' ? (
-            <>
-              <div>
+    <div>
+      <div>
+        <label>
+          Number of Sections:
+          <input
+            type="number"
+            value={numSections}
+            onChange={handleNumSectionsChange}
+            min="1"
+          />
+          <button onClick={() => setNumSections(numSections + 1)}>+</button>
+          <button onClick={() => setNumSections(Math.max(1, numSections - 1))}>
+            -
+          </button>
+        </label>
+      </div>
+      {sectionData.map((section) => (
+        <div key={section.id}>
+          <form>
+            {section.parts.map((item, index) => (
+              <div key={index}>
                 <label>
-                  Question:
-                  <input
-                    type="text"
-                    value={item.question}
+                  Type:
+                  <select
+                    value={item.type}
                     onChange={(e) =>
-                      handleInputChange(index, 'question', e.target.value)
+                      handleSelectChange(section.id, index, e.target.value)
                     }
-                  />
+                  >
+                    <option value="qa">Question & Answer</option>
+                    <option value="content">Content</option>
+                  </select>
                 </label>
+                {item.type === 'qa' ? (
+                  <>
+                    <div>
+                      <label>
+                        Question:
+                        <input
+                          type="text"
+                          value={item.question}
+                          onChange={(e) =>
+                            handleInputChange(
+                              section.id,
+                              index,
+                              'question',
+                              e.target.value
+                            )
+                          }
+                        />
+                      </label>
+                    </div>
+                    <div>
+                      <label>
+                        Answer:
+                        <input
+                          type="text"
+                          value={item.answer}
+                          onChange={(e) =>
+                            handleInputChange(
+                              section.id,
+                              index,
+                              'answer',
+                              e.target.value
+                            )
+                          }
+                        />
+                      </label>
+                    </div>
+                  </>
+                ) : (
+                  <div>
+                    <label>
+                      Content:
+                      <input
+                        type="text"
+                        value={item.content}
+                        onChange={(e) =>
+                          handleInputChange(
+                            section.id,
+                            index,
+                            'content',
+                            e.target.value
+                          )
+                        }
+                      />
+                    </label>
+                  </div>
+                )}
               </div>
-              <div>
-                <label>
-                  Answer:
-                  <input
-                    type="text"
-                    value={item.answer}
-                    onChange={(e) =>
-                      handleInputChange(index, 'answer', e.target.value)
-                    }
-                  />
-                </label>
-              </div>
-            </>
-          ) : (
-            <div>
-              <label>
-                Content:
-                <input
-                  type="text"
-                  value={item.content}
-                  onChange={(e) =>
-                    handleInputChange(index, 'content', e.target.value)
-                  }
-                />
-              </label>
-            </div>
-          )}
+            ))}
+          </form>
         </div>
       ))}
-      <button type="submit">Submit</button>
-    </form>
+      <button onClick={handleSubmit}>Submit</button>
+    </div>
   );
 };
 
