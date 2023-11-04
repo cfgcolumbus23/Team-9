85f02043bf5de0f875182317116f78f4155edc53
TonyLiou4 <tonyliou4@gmail.com>
2023-11-04 09:57:31 -0400

    Add provideres page image

--- client/src/app/provider-page/select-games/page.jsx
+++ client/src/app/provider-page/select-games/page.jsx
@@ -1,22 +1,29 @@
-import Image from 'next/image';
+import Image from "next/image";
+import styles from "./styles/globalStyles.css"; // Make sure this CSS file exists and is properly configured
+import mono from "./images/mono.jpeg";
+import minecraft from "./images/minecraft.jpeg";
+import rgb from "./images/rgb.jpeg";
+import React from "react";
 
-import styles from './styles/globalStyles.css';
-
-import React from 'react';
 //fix
 export default function Desktop() {
   return (
     <div
       className={styles.container}
       style={{
-        backgroundImage: `url('https://images.pexels.com/photos/743986/pexels-photo-743986.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')`,
-        backgroundSize: 'cover',
-        backgroundPosition: 'center',
+        backgroundImage:
+          'url("https://images.pexels.com/photos/743986/pexels-photo-743986.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1")',
+        backgroundSize: "cover",
+        backgroundPosition: "center",
       }}
     >
       <div className="max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8 py-6">
         <div className="flex items-center justify-between">
-          {/* <Image src={Logo} alt="Logo" className="h-10 w-auto" /> */}
+          <img
+            src="https://futurereadyfive.org/wp-content/uploads/2022/08/FR5-horizontal-logo.png"
+            alt="Future Ready Five Logo"
+            className="w-[25%]"
+          />
           <h1 className="text-2xl md:text-3xl lg:text-5xl font-bold">
             Customize Your Learning!
           </h1>
@@ -33,7 +40,7 @@ export default function Desktop() {
 
         <div className="grid grid-cols-1 md:grid-cols-3 gap-8 mt-8">
           <div className="bg-white p-6 rounded-lg shadow-md">
-            {/* <Image src={Parents} alt="Parent" className="rounded-t-lg mb-4" /> */}
+            <Image src={mono} alt="candy" className="rounded-t-lg mb-4" />
             <h2 className="my-5 text-lg md:text-xl">
               View your child's progress!
             </h2>
@@ -49,7 +56,13 @@ export default function Desktop() {
           </div>
 
           <div className="bg-white p-6 rounded-lg shadow-md">
-            {/* <Image src={Children} alt="Student" className="rounded-t-lg mb-4" /> */}
+            {
+              <Image
+                src={minecraft}
+                alt="minecraft"
+                className="rounded-t-lg mb-4"
+              />
+            }
             <h2 className="my-5 text-lg md:text-xl">
               Play games and view progress!
             </h2>
@@ -65,11 +78,7 @@ export default function Desktop() {
           </div>
 
           <div className="bg-white p-6 rounded-lg shadow-md">
-            {/* <Image
-              src={Provider}
-              alt="Provider"
-              className="rounded-t-lg mb-4"
-            /> */}
+            {<Image src={rgb} alt="RPG" className="rounded-t-lg mb-4" />}
             <h2 className="my-5 text-lg md:text-xl">Create learning game!</h2>
             <a
               href="http://localhost:3000/provider-page"
@@ -85,7 +94,11 @@ export default function Desktop() {
 
         <div className="mt-8 bg-[#005959]">
           <div className="flex justify-between items-center mb-4">
-            {/* <Image src={LogoWhite} alt="Logo" className="h-10 w-auto" /> */}
+            <img
+              src="https://futurereadyfive.org/wp-content/uploads/2022/08/foot-logo03@4x.png"
+              alt="Future Ready Five"
+              className="w-[25%]"
+            />
             <div className="text-white">
               <p>215 North Front St, Ste 620</p>
               <p>Columbus, Ohio 43215</p>
