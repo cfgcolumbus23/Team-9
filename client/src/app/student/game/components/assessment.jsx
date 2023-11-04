5906149163bea0d75de2ff9092535728bf969951
Inesh Loka <56318873+inesh-l@users.noreply.github.com>
2023-11-04 08:16:23 -0400

    Deleted unused code

--- client/src/app/student/game/components/assessment.jsx
+++
@@ -1,9 +0,0 @@
-export function Assessment() {
-    return (
-        <div>
-            <svg width="106" height="100" viewBox="0 0 106 100" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M53 0L65.3483 38.0041H105.308L72.9799 61.4919L85.3282 99.4959L53 76.0081L20.6718 99.4959L33.0201 61.4919L0.691891 38.0041H40.6517L53 0Z" fill="#D9D9D9"/>
-            </svg> 
-        </div>
-    )
-}
--- client/src/app/student/game/components/checkpoint.jsx
+++
@@ -1,8 +0,0 @@
-export function Checkpoint(props) {
-    let checkpointClass = (props.type == "quiz") ? "rounded-full" : ""; 
-    return (
-        <div class={"flex flex-col justify-center w-[95px] h-[95px] hover " + props.color + " " + checkpointClass}>
-            {"Question " + props.number}
-        </div>
-    )
-}
