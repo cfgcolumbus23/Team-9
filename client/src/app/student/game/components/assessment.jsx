03c7b85d9663b360dbd6b57d26b0aec49c8c6740
Inesh Loka <56318873+inesh-l@users.noreply.github.com>
2023-11-04 06:09:04 -0400

    Implemented basic game progression

--- client/src/app/student/game/components/assessment.jsx
+++ client/src/app/student/game/components/assessment.jsx
@@ -1,8 +1,8 @@
 export function Assessment() {
     return (
         <div>
-            <svg width="143" height="88" viewBox="0 0 143 88" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M1 1.00003C90.6466 4.30554 119.611 24.9255 142 87.5" stroke="black" stroke-dasharray="2 2"/>
+            <svg width="106" height="100" viewBox="0 0 106 100" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M53 0L65.3483 38.0041H105.308L72.9799 61.4919L85.3282 99.4959L53 76.0081L20.6718 99.4959L33.0201 61.4919L0.691891 38.0041H40.6517L53 0Z" fill="#D9D9D9"/>
             </svg> 
         </div>
     )
--- client/src/app/student/game/components/checkpoint.jsx
+++ client/src/app/student/game/components/checkpoint.jsx
@@ -1,7 +1,7 @@
 export function Checkpoint(props) {
     let checkpointClass = (props.type == "quiz") ? "rounded-full" : ""; 
     return (
-        <div class={"w-[95px] h-[95px] hover " + props.color + " " + checkpointClass}>
+        <div class={"flex flex-col justify-center w-[95px] h-[95px] hover " + props.color + " " + checkpointClass}>
             {"Question " + props.number}
         </div>
     )
