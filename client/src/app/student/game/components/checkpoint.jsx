b89ae3c8cee134c9e9740ec2efa5f19d9fe6cc5c
LeoZhang47 <zleo200347@gmail.com>
2023-11-04 09:44:08 -0400

    styling

--- client/src/app/student/game/components/checkpoint.jsx
+++ client/src/app/student/game/components/checkpoint.jsx
@@ -1,7 +1,7 @@
 export function Checkpoint(props) {
     let checkpointClass = (props.type == "quiz") ? "rounded-full" : ""; 
     return (
-        <div class={"flex flex-col justify-center w-[95px] h-[95px] hover " + props.color + " " + checkpointClass}>
+        <div class={"flex flex-col justify-center border-solid border-2 border-white-500  w-[95px] h-[95px] hover " + props.color + " " + checkpointClass}>
             {"Question " + props.number}
         </div>
     )
